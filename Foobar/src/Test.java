87c62e3e5d1a6e65bd01f68fcb55c064be3bf280
traffic144 <traffic144@gmail.com>
2014-04-30 15:05:04 +0200

    reconstitution depuis GitHub faite

--- Foobar/src/Test.java
+++ Foobar/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("foobar stands for f*cked up beyond all recognition");
 		System.out.println("foobar stands for f*cked up beyond all recognition2");
+		System.out.println("foobar stands for f*cked up beyond all recognition3");
 	}
 
 }
