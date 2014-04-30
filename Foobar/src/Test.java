c66251d3a5fdc1edf00ab11e74ca2f084ab2be27
traffic144 <traffic144@gmail.com>
2014-04-30 14:57:00 +0200

    first commit

---
+++ Foobar/src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("foobar stands for f*cked up beyond all recognition");
+		System.out.println("foobar stands for f*cked up beyond all recognition2");
+	}
+
+}
